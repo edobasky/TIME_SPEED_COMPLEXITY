4c80a9f9e9df5ac400c115325074f4fab2e3b91b
edobasky <edore7@gmail.com>
2026-06-17 04:19:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using SORTING;
+
+internal class Program
+{
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Algo Suckers!");
+            #region bubble sort implementation
+
+            BubbleSort sort = new();
+            var arr = new int[] { 23, 41, 25, 54, 18, 51 };
+            var result = sort.sortImp(arr);
+
+            foreach (var item in result)
+            {
+                Console.Write(item);
+                Console.Write("||");
+            }
+            Console.WriteLine();
+            #endregion
+        }
+}
---
+++ SORTING/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SORTING
+{
+    public class BubbleSort
+    {
+        /*
+         Here 3 things are kept track off,
+        1. the i iterator, 2. the j(i + 1) iterator and then the wall
+          which separates unsorted from sorted, as every sort it left
+        on the other side of the wall........for the inner loop
+        i and i+1 is checked withing the boundaries of the current wall and swapped if i is 
+        greater than (i + 1)....
+         */
+        public int[] sortImp(int[] array)
+        {
+            for (int a = array.Length - 1; a > 0; a--)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+            return array;
+        }
+
+        private void Swap(int[] arr, int i, int y)
+        {
+            if (i == y) return;
+
+            int temp = arr[i];
+            arr[i] = arr[y];
+            arr[y] = temp;
+        }
+    }
+
+}
